b4581b045f1d6b39c066eb283bc5575b2f4b0f79
Rana Umair <rjee4631@gmail.com>
2022-05-06 11:46:53 +0500

    Create car_companies_name_array.php

---
+++ car_companies_name_array.php
@@ -0,0 +1,6 @@
+<?php 
+
+$temp___car_companies_name = ["Daihatsu", "Dodge", "Donkervoort", "DS", "Ferrari", 'Fiat' , 'Fisker' , 'Ford' , 'Honda' , 'Hummer' , 'Hyundai' , 'Infiniti' , 'Iveco' , 'Jaguar' , 'Jeep' , 'Kia' , 'KTM' 
+echo json_encode( $temp___car_companies_name );
+
+?>

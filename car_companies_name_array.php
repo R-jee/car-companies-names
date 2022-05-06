5de42f43c5b2882f4a5c8f23326d445fff944aea
Rana Umair <rjee4631@gmail.com>
2022-05-06 11:50:14 +0500

    Update car_companies_name_array.php

--- car_companies_name_array.php
+++ car_companies_name_array.php
@@ -1,6 +1,6 @@
 <?php 
 
-$temp___car_companies_name = ["Daihatsu", "Dodge", "Donkervoort", "DS", "Ferrari", 'Fiat' , 'Fisker' , 'Ford' , 'Honda' , 'Hummer' , 'Hyundai' , 'Infiniti' , 'Iveco' , 'Jaguar' , 'Jeep' , 'Kia' , 'KTM'  , 'Lada'  , 'Lamborghini'  , 'Lancia'  , 'Land Rover'  , 'Landwind', 'Lexus'  , 'Lotus'  , 'Maserati'  , 'Maybach'  , 'Mazda'  , 'McLaren'  , 'Mercedes-Benz'  , 'MG'  , 'Mini'  , 'Mitsubishi'  , 'Morgan'  , 'Nissan'  , 'Opel'  , 'Peugeot'  , 'Porsche'  , 'Renault'  , 'Rolls-Royce'  , 'Rover'  , 'Saab'  , 'Seat'  , 'Skoda'  , 'Smart'  , 'SsangYong'  , 'Subaru'  , 'Suzuki'  , 'Tesla'  , 'Toyota'  , 'Volkswagen'  , 'Volvo'  , 'Audi'  , 'BMW'  , 'Buick'  , 'Cadillac'  , 'Chevrolet'  , 'Chrysler'  , 'GM'  , 'GEM'  , 'GMC'  , 'Isuzu'  , 'Lincoln'  ,  'Mercury'  , 'Oldsmobile'  , 'Pontiac'  , 'Regal'  , 'Saturn'];
+$temp___car_companies_name = ["Daihatsu","Dodge","Donkervoort","DS","Ferrari","Fiat","Fisker","Ford","Honda","Hummer","Hyundai","Infiniti","Iveco","Jaguar","Jeep","Kia","KTM","Lada","Lamborghini","Lancia","Land Rover","Landwind","Lexus","Lotus","Maserati","Maybach","Mazda","McLaren","Mercedes-Benz","MG","Mini","Mitsubishi","Morgan","Nissan","Opel","Peugeot","Porsche","Renault","Rolls-Royce","Rover","Saab","Seat","Skoda","Smart","SsangYong","Subaru","Suzuki","Tesla","Toyota","Volkswagen","Volvo","Audi","BMW","Buick","Cadillac","Chevrolet","Chrysler","GM","GEM","GMC","Isuzu","Lincoln","Mercury","Oldsmobile","Pontiac","Regal","Saturn"];
 echo json_encode( $temp___car_companies_name );
 
 ?>

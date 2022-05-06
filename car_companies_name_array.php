48809092fed9c2eba7ad1a8766840aa61868ac10
Rana Umair <rjee4631@gmail.com>
2022-05-06 11:59:59 +0500

    Update car_companies_name_array.php

--- car_companies_name_array.php
+++ car_companies_name_array.php
@@ -3,4 +3,7 @@
 $temp___car_companies_name = ["Daihatsu","Dodge","Donkervoort","DS","Ferrari","Fiat","Fisker","Ford","Honda","Hummer","Hyundai","Infiniti","Iveco","Jaguar","Jeep","Kia","KTM","Lada","Lamborghini","Lancia","Land Rover","Landwind","Lexus","Lotus","Maserati","Maybach","Mazda","McLaren","Mercedes-Benz","MG","Mini","Mitsubishi","Morgan","Nissan","Opel","Peugeot","Porsche","Renault","Rolls-Royce","Rover","Saab","Seat","Skoda","Smart","SsangYong","Subaru","Suzuki","Tesla","Toyota","Volkswagen","Volvo","Audi","BMW","Buick","Cadillac","Chevrolet","Chrysler","GM","GEM","GMC","Isuzu","Lincoln","Mercury","Oldsmobile","Pontiac","Regal","Saturn"];
 echo json_encode( $temp___car_companies_name );
 
+$lowercase___car_companies_name = ["daihatsu","dodge","donkervoort","ds","ferrari","fiat","fisker","ford","honda","hummer","hyundai","infiniti","iveco","jaguar","jeep","kia","ktm","lada","lamborghini","lancia","land rover","landwind","lexus","lotus","maserati","maybach","mazda","mclaren","mercedes-benz","mg","mini","mitsubishi","morgan","nissan","opel","peugeot","porsche","renault","rolls-royce","rover","saab","seat","skoda","smart","ssangyong","subaru","suzuki","tesla","toyota","volkswagen","volvo","audi","bmw","buick","cadillac","chevrolet","chrysler","gm","gem","gmc","isuzu","lincoln","mercury","oldsmobile","pontiac","regal","saturn"];
+echo json_encode( $lowercase___car_companies_name );
+
 ?>
